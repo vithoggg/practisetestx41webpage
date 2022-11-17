bf0c0bcc8b9d73fbb0691b3ab80777897069f1b9
vithoggg <59486714+vithoggg@users.noreply.github.com>
2022-11-17 02:34:13 +0200

    Add files via upload

---
+++ nextIT/bgapp.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x3fa4ff,
+  maxDistance: 24.00,
+  spacing: 16.00,
+  showDots: false
+})
